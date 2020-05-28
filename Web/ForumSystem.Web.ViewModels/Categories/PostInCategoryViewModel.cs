320144371bd1095750510d5769753a359963ceb0
Sevdalin Vasilev <predator187@abv.bg>
2020-05-28 23:46:16 +0300

    Added fontawesome icons

--- Web/ForumSystem.Web.ViewModels/Categories/PostInCategoryViewModel.cs
+++ Web/ForumSystem.Web.ViewModels/Categories/PostInCategoryViewModel.cs
@@ -2,9 +2,12 @@
 {
     using ForumSystem.Data.Models;
     using ForumSystem.Services.Mapping;
+    using System;
 
     public class PostInCategoryViewModel : IMapFrom<Post>
     {
+        public DateTime CreatedOn { get; set; }
+
         public string Title { get; set; }
 
         public string UserUserName { get; set; }
