c8d153f3374d93eac4ec2014ffadade47363629c
Sevdalin Vasilev <predator187@abv.bg>
2020-05-28 23:01:39 +0300

    Improved code quality

--- Services/ForumSystem.Services.Data/CategoriesService.cs
+++ Services/ForumSystem.Services.Data/CategoriesService.cs
@@ -18,15 +18,26 @@
 
         public IEnumerable<T> GetAll<T>(int? count = null)
         {
-          IQueryable<Category> query = this.categoriesRepository.All()
-                .OrderBy(x => x.Name);
+            IQueryable<Category> query = this.categoriesRepository.All()
+                  .OrderBy(x => x.Name);
 
-          if (count.HasValue)
+            if (count.HasValue)
             {
                 query = query.Take(count.Value);
             }
 
-          return query.To<T>().ToList();
+            return query.To<T>().ToList();
+        }
+
+        public T GetByName<T>(string name)
+        {
+            var category = this.categoriesRepository
+                 .All()
+                 .Where(x => x.Name == name)
+                 .To<T>()
+                 .FirstOrDefault();
+
+            return category;
         }
     }
 }
