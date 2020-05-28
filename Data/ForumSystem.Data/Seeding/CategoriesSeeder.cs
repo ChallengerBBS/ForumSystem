88f607527bdb5a3d9feda64a11f41156de330181
Sevdalin Vasilev <predator187@abv.bg>
2020-05-28 20:07:37 +0300

    Added Categories seeder

---
+++ Data/ForumSystem.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,30 @@
+﻿namespace ForumSystem.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using ForumSystem.Data.Models;
+    using Microsoft.EntityFrameworkCore.Internal;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (!dbContext.Categories.Any())
+            {
+                var categories = new List<string> { "Sport", "Coronavirus", "News", "Music", "Cats" };
+
+                foreach (var category in categories)
+                {
+                    await dbContext.Categories.AddRangeAsync(new Category
+                    {
+                        Name = category,
+                        Description = category,
+                        Title = category,
+                    });
+                }
+            }
+        }
+    }
+}
