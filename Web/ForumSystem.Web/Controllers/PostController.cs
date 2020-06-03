4aa4cb2bb07426f9f70676f724ae718094aac051
Sevdalin Vasilev <predator187@abv.bg>
2020-06-04 01:03:10 +0300

    Added posts create logic

---
+++ Web/ForumSystem.Web/Controllers/PostController.cs
@@ -0,0 +1,45 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using AspNetCore;
+    using ForumSystem.Data.Common.Repositories;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.ViewModels.Posts;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PostController : Controller
+    {
+        private readonly IDeletableEntityRepository<Post> postsRepository;
+
+        public PostController(IDeletableEntityRepository<Post> postsRepository)
+        {
+            this.postsRepository = postsRepository;
+        }
+
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult Create(PostCreateInputModel input)
+        {
+            if(!ModelState.IsValid)
+            {
+
+            return View(input);
+            }
+
+            var post = new Post
+            {
+                CategoryId = input.CategoryId,
+                Content = input.Content,
+                Title = input.Title,
+                User=
+            };
+
+        }
+    }
+}
