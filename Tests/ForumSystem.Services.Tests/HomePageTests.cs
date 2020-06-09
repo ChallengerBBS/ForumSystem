cc77ecd5ad5488ead6e3e6a69d05360a09d5dc71
Sevdalin Vasilev <predator187@abv.bg>
2020-06-09 23:21:56 +0300

    Added integration test on homepage

---
+++ Tests/ForumSystem.Services.Tests/HomePageTests.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumSystem.Services.Tests
+{
+    using ForumSystem.Web;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Microsoft.CodeAnalysis;
+    using System.Threading.Tasks;
+    using Xunit;
+    public class HomePageTests
+    {
+        [Fact]
+        public async Task HomePageShouldHaveH1Title()
+        {
+            var serverFactory = new WebApplicationFactory<Startup>();
+            var client = serverFactory.CreateClient();
+
+            var response = await client.GetAsync("/");
+            var responseAsync = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("<h1", responseAsync);
+
+        }
+    }
+}
