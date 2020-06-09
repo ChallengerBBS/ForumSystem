4a985fd2e81436ac2c9cbc49a696343fc55e4cbe
Sevdalin Vasilev <predator187@abv.bg>
2020-06-09 23:52:19 +0300

    Added Selenium test for homepage

---
+++ Tests/ForumSystem.Services.Tests/SeleniumHomePageTests.cs
@@ -0,0 +1,29 @@
+﻿namespace ForumSystem.Services.Tests
+{
+    using ForumSystem.Web;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Remote;
+    using Xunit;
+    public class SeleniumHomePageTests
+    {
+        private RemoteWebDriver browser;
+        private SeleniumServerFactory<Startup> serverFactory;
+
+        public SeleniumHomePageTests()
+        {
+            this.serverFactory = new SeleniumServerFactory<Startup>();
+            serverFactory.CreateClient();
+            var options = new ChromeOptions();
+            options.AddArguments("--headless", "--ignore-certificate-errors");
+
+            this.browser = new RemoteWebDriver(options);
+        }
+
+        [Fact]
+        public void HomePageShouldHaveH1Tag()
+        {
+            this.browser.Navigate().GoToUrl(this.serverFactory.RootUri + "/Home/Index");
+            Assert.Contains("Welcome to", this.browser.FindElementByCssSelector("h1").Text);
+        }
+    }
+}
