b6ee4085670454aa7cc7739162256bcfeb8209c4
Sevdalin Vasilev <predator187@abv.bg>
2020-06-12 20:08:45 +0300

    Added comments view and logic

---
+++ Services/ForumSystem.Services.Data/CommentsService.cs
@@ -0,0 +1,30 @@
+﻿namespace ForumSystem.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using ForumSystem.Data.Common.Repositories;
+    using ForumSystem.Data.Models;
+
+    public class CommentsService : ICommentsService
+    {
+        private readonly IDeletableEntityRepository<Comment> commentsRepository;
+
+        public CommentsService(IDeletableEntityRepository<Comment> commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        public async Task Create(int postId, string userId, string content, int? parentId = null)
+        {
+            var comment = new Comment
+            {
+                Content = content,
+                ParentId = parentId,
+                PostId = postId,
+                UserId = userId,
+            };
+            await this.commentsRepository.AddAsync(comment);
+            await this.commentsRepository.SaveChangesAsync();
+        }
+    }
+}
