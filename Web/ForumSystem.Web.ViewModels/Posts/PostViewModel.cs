cf58320d35c44b1e2d484bde0878451dca476cea
ChallengerBBS <predator187@abv.bg>
2020-06-15 00:28:24 +0300

    Fixed issue with posts' comments

--- Web/ForumSystem.Web.ViewModels/Posts/PostViewModel.cs
+++ Web/ForumSystem.Web.ViewModels/Posts/PostViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace ForumSystem.Web.ViewModels.Posts
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Text;
+
     using AutoMapper;
     using ForumSystem.Data.Models;
     using ForumSystem.Services.Mapping;
@@ -10,6 +13,7 @@
     public class PostViewModel : IMapFrom<Post>, IMapTo<Post>, IHaveCustomMappings
     {
         public int Id { get; set; }
+
         public DateTime CreatedOn { get; set; }
 
         public string Title { get; set; }
@@ -22,13 +26,15 @@
 
         public int VotesCount { get; set; }
 
+        public IEnumerable<PostCommentViewModel> Comments { get; set; }
+
         public void CreateMappings(IProfileExpression configuration)
         {
             configuration.CreateMap<Post, PostViewModel>()
-                  .ForMember(x => x.VotesCount, options =>
-                    {
-                        options.MapFrom(p => p.Votes.Sum(v => (int)v.Type));
-                  });
+                .ForMember(x => x.VotesCount, options =>
+                {
+                    options.MapFrom(p => p.Votes.Sum(v => (int)v.Type));
+                });
         }
     }
 }
